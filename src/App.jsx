2c72415ca5eb20acf309ac5ef3e5ed35deb1bc52
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-12-01 22:21:00 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './components/SharedLayot';
 import { RestrictedRoute } from './components/RestrictedRoute';
 import { PrivateRoute } from './components/PrivateRoute';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { refreshUser } from './redux/operations';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const { isRefreshUser } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
 
   const HomePage = lazy(() => import('./pages/HomePage'));
   const ContactsPage = lazy(() => import('./pages/ContactsPage'));
   const LoginPage = lazy(() => import('./pages/LoginPage'));
   const RegisterPage = lazy(() => import('./pages/RegisterPage'));
+
+  if (isRefreshUser) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
